c2f62cea6136ad5a25747aef0e6e3b2a05e8ed47
NX <976180942@qq.com>
2024-01-26 18:08:52 +0800

    fix: Failed to copy to clipboard

--- internal/module/url/init.go
+++ internal/module/url/init.go
@@ -17,8 +17,8 @@ func (u *ModuleUrl) GetName() string {
 
 func (u *ModuleUrl) Init() {
 	if config.Get().Port == "80" {
-		baseURL = "http://" + config.Get().Host + config.Get().Prefix + "api/"
+		baseURL = "https://" + config.Get().Host + config.Get().Prefix + "api/"
 	} else {
-		baseURL = "http://" + config.Get().Host + ":" + config.Get().Port + config.Get().Prefix + "api/"
+		baseURL = "https://" + config.Get().Host + ":" + config.Get().Port + config.Get().Prefix + "api/"
 	}
 }
